72093598ce1bdba5d0bf03061ecae2e5471965d3
Tess Lameyer <tjlameyer@gmail.com>
2019-12-19 14:44:58 -0800

    tests written for login route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -20,7 +20,7 @@ describe('app routes', () => {
     return mongoose.connection.close();
   });
 
-  it('can sigup a new user', () => {
+  it('can signup a new user', () => {
     return request(app)
       .post('/api/v1/auth/signup')
       .send({ email: 'tess@tess.com', password: 'realpassword' })
@@ -31,10 +31,48 @@ describe('app routes', () => {
           __v: 0
         });
       });
-
   });
 
+  it('can login a user', async() => {
+    //want to start out with an exisiting user in the database 
+    const user = await User.create({ email: 'tess@tess.com', password: 'realpassword' });
+    //now do the test 
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ email: 'tess@tess.com', password: 'realpassword' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: user.id, 
+          email: 'tess@tess.com',
+          __v: 0
+        });
+      });
+  });
 
+  it('throws error due to invalid email login', async() => {
+    await User.create({ email: 'tess@tess.com', password: 'realpassword' });
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ email: 'nottess@tess.com', password: 'realpassword' })
+      .then(res => {
+        expect(res.body).toEqual({
+          message: 'invalid email or password', 
+          status: 401
+        });
+      });
+  });
 
+  it('throws error due to invalid password login', async() => {
+    await User.create({ email: 'tess@tess.com', password: 'realpassword' });
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ email: 'tess@tess.com', password: 'fakepassword' })
+      .then(res => {
+        expect(res.body).toEqual({
+          message: 'invalid email or password', 
+          status: 401
+        });
+      });
+  });
 
 });
